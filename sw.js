a968e6c8fb680dd8284b0a3145bf8a5b57655dba
wasimahmedqureshi <wasimahmedqureshi@gmail.com>
2026-05-23 17:43:45 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = 'rccms-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
